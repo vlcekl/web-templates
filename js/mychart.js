45aa781c27399b9a09c11260944ea57a799d9124
Lukas Vlcek <lukas_vlcek@yahoo.com>
2021-10-28 15:38:43 -0500

    added leaflet

--- js/mychart.js
+++ js/mychart.js
@@ -11,7 +11,7 @@ function draw(id) {
   }
 }
 
-draw('mycanvas');
+//draw('mycanvas');
 draw('mycanvas2');
 
 // get data
@@ -53,3 +53,24 @@ radios.forEach(radio => radio.addEventListener('change', (event) => {
     const charts = document.querySelectorAll(`#${sec} .ct-chart`);
     charts.forEach(c => c.__chartist__.update())
 }))
+
+//var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var mymap = L.map('mapid').setView([37.8, -96], 4);
+
+
+/*
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
+*/
+
+var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(mymap);
+
