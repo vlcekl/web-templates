cd1c5a7aa0c5aca1bd3922a573143be5656bfee3
Lukas Vlcek <lukas_vlcek@yahoo.com>
2022-11-25 19:47:48 -0600

    finished calendar

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -15,27 +15,27 @@ function getCalendarData(startDate, endDate) {
     // Month names
     const monthName = [
         'January', 'February', 'March', 'April', 'May', 'June', 'July',
-         'August', 'September', 'October', 'November', 'December'
+        'August', 'September', 'October', 'November', 'December'
     ];
 
     // Return all months/years between start and end dates
     // Take date range and create calendar
     const dateStart = new Date(startDate);
     const dateEnd = new Date(endDate);
- 
+
     // Years 
     const firstYear = dateStart.getFullYear();
     const nYear = dateEnd.getFullYear() - firstYear;
- 
+
     // Months
     const firstMonth = dateStart.getMonth();
-    const nMonth = dateEnd.getMonth() - firstMonth + 1 + 12*nYear;
- 
+    const nMonth = dateEnd.getMonth() - firstMonth + 1 + 12 * nYear;
+
     // Create array of year&month items to display in calendar
     monthData = [];
     for (let i = firstMonth; i < firstMonth + nMonth; i++) {
-      monthData.push([firstYear + Math.floor(i / 12) ,
-                       i % 12, monthName[i % 12]]);
+        monthData.push([firstYear + Math.floor(i / 12),
+        i % 12, monthName[i % 12]]);
     }
 
     // Create array of first/last weekdays for each month
@@ -47,34 +47,40 @@ function getCalendarData(startDate, endDate) {
         return [x.getUTCDay(), monthLength(m[1], m[0])];
     })
 
-    return {"months": monthData, "days": dayData};
+    return { "months": monthData, "days": dayData };
 }
 
 // Create a calendar element using html template of month
 function createCalendar(calendar, data) {
 
-  const monthTemplate = calendar.querySelector('template');
-  const months = document.createDocumentFragment();
+    const monthTemplate = calendar.querySelector('template');
+    const months = document.createDocumentFragment();
 
-  // Generate months from templates
-  data.months.forEach((m, i) => {
-    let clon = monthTemplate.content.cloneNode(true);
+    // Generate months from templates
+    data.months.forEach((m, i) => {
+        let clon = monthTemplate.content.cloneNode(true);
 
-    // Set title to Month/Year
-    let title = clon.querySelector('.calendar__title');
-    title.textContent = `${monthName[m[1]]} ${m[0]}`;
+        // Set title to Month/Year
+        let title = clon.querySelector('.calendar__title');
+        title.textContent = `${m[2]} ${m[0]}`;
 
-    // Add days with their numbers into correct day divs
-    let daysdiv = clon.querySelector('.calendar__days');
+        // Add days with their numbers into correct day divs
+        let daysdiv = clon.querySelector('.calendar__days');
 
-    let monthDayDivs = [];
+        let mDays = [];
+        for (let j = 0; j < data.days[i][0]; j++) {
+            mDays.push(`<div></div>`);
+        }
+        for (let j = 1; j <= data.days[i][1]; j++) {
+            mDays.push(`<div data-year=${m[0]} data-month=${m[1]}>${j}</div>`);
+        }
 
-    daysdiv.innerHTML = monthDayDivs;
+        daysdiv.innerHTML = mDays.join('');
 
-    months.appendChild(clon);
-  });
+        months.appendChild(clon);
+    });
 
-  calendar.appendChild(months);
+    calendar.appendChild(months);
 }
 
 // Run code
@@ -83,10 +89,13 @@ const calData = getCalendarData('2022-10-26 CDT', '2023-01-15 CDT');
 
 const calendar = document.querySelector('.calendar');
 
+// Create calendar content using month template
+createCalendar(calendar, calData);
+
 // Add click listener to calendar 
 calendar.addEventListener('click', e => {
-    console.log(e.target.id);
-})
-
-// Create calendar using month template
-createCalendar(calendar = calendar, data = calData);
+    let year = parseInt(e.target.dataset.year);
+    let month = parseInt(e.target.dataset.month);
+    let day = parseInt(e.target.textContent);
+    console.log(year, month+1, day);
+});
